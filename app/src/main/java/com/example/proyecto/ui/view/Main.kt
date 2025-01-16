9e8249576aa55feebed6eda1e878c23e847cc03a
Lucas <lucas.taladrid@hotmail.es>
2025-01-16 14:08:24 +0100

    Diagrama de base de datos

--- app/src/main/java/com/example/proyecto/ui/view/Main.kt
+++ app/src/main/java/com/example/proyecto/ui/view/Main.kt
@@ -9,7 +9,7 @@ import com.example.proyecto.data.routes.Routes
 @Composable
 fun Main(){
     val navController= rememberNavController()
-    NavHost(navController=navController, startDestination = Routes.Pantalla1.route){
+    NavHost(navController=navController, startDestination = Routes.Login.route){
         composable(Routes.Login.route){Login(navController)}
         composable(Routes.Register.route){ RegisterScreen(navController) }
         composable(Routes.Pantalla1.route) { Pantalla1(navController) }
